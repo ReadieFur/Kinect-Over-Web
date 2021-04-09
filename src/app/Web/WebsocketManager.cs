dbfe214839ce61dec36ef70a7ab32e836a707df8
kOF.Readie <kof.readie@outlook.com>
2021-04-09 23:21:14 +0100

    Wrote WebSocket classes

---
+++ src/app/Web/WebsocketManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace KinectOverWeb.Web
+{
+    public class WebsocketManager
+    {
+        public bool enabled { private set; get; } = false;
+
+        public readonly Protocol protocol;
+        public readonly System.Net.IPAddress ip;
+        public readonly int port;
+
+        private WebSocketServer server;
+
+        public WebsocketManager(/*Protocol _protocol,*/ System.Net.IPAddress _ip, int _port)
+        {
+            protocol = Protocol.ws;
+            ip = _ip;
+            port = _port;
+            server = new WebSocketServer($"{protocol}://{ip}:{port}");
+        }
+
+        public void StartServer()
+        {
+            server.Start();
+            enabled = true;
+        }
+
+        public void StopServer()
+        {
+            server.Stop();
+            enabled = false;
+        }
+
+        public WebSocketServiceHost AddEndpoint(string _path, Action<WebSocketBehavior> _initializer = null)
+        {
+            if (server.WebSocketServices.Hosts.FirstOrDefault(_host => _host.Path == _path) != null) { return null; }
+            
+            server.WebSocketServices.AddService<WebsocketService>(_path, _initializer);
+
+            /*WebSocket websocket = new WebSocket($"{protocol}://{ip}:{port}{_path}");
+            while (!websocket.IsAlive) { websocket.Connect(); }
+            websocket.Close();*/
+
+            return server.WebSocketServices.Hosts.FirstOrDefault(_host => _host.Path == _path);
+        }
+
+        public bool RemoveEndpoint(string _path)
+        {
+            if (server.WebSocketServices.Hosts.FirstOrDefault(_host => _host.Path == _path) == null) { return false; }
+            server.WebSocketServices.RemoveService(_path);
+            return true;
+        }
+
+        public WebSocketServiceHost GetEndpoint(string _path)
+        {
+            return server.WebSocketServices.Hosts.FirstOrDefault(_host => _host.Path == _path);
+        }
+
+        public enum Protocol
+        {
+            ws = 0,
+            wss = 1
+        }
+    }
+}
---
+++ src/app/Web/WebsocketService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace KinectOverWeb.Web
+{
+    public class WebsocketService : WebSocketBehavior
+    {
+        public event Action<WebsocketService> onOpen;
+        public event Action<WebsocketService, MessageEventArgs> onMessage;
+        public event Action<WebsocketService, CloseEventArgs> onClose;
+        public event Action<WebsocketService, ErrorEventArgs> onError;
+
+        protected override void OnOpen()
+        {
+            if (onOpen != null) { onOpen(this); }
+        }
+
+        protected override void OnMessage(MessageEventArgs _message)
+        {
+            if (onMessage != null) { onMessage(this, _message); }
+        }
+
+        protected override void OnClose(CloseEventArgs _close)
+        {
+            if (onClose != null) { onClose(this, _close); }
+        }
+
+        protected override void OnError(ErrorEventArgs _error)
+        {
+            if (onError != null) { onError(this, _error); }
+        }
+    }
+}
