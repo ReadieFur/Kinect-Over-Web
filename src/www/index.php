487c85949d499127e7ad989346d3c8d3af0dfb61
kOF.Readie <kof.readie@outlook.com>
2021-04-10 18:33:54 +0100

    Build web home page

--- src/www/index.php
+++ src/www/index.php
@@ -16,7 +16,37 @@
 </head>
 <header id="header"><?php echo execAndRead("{$SITE_ROOT}/assets/php/header.php"); ?></header>
 <body>
-
+    <div>
+        <section id="splashTitleContainer">
+            <div class="center">
+                <h1>Kinect Over Web</h1>
+                <h4>View the sources that your kinect is outputting</h4>
+            </div>
+        </section>
+        <section>
+            <h3>Sources</h3>
+            <hr>
+            <br>
+            <table id="sourcesList">
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="bottomStrip">
+                                <a href="<?php echo $WEB_ROOT ?>/view-source/Body_Points_Mapped_To_Colour/">
+                                    <h4>Body points mapped to colour</h4>
+                                    <p>
+                                        Draws the skeletons of the found bodies with their positions mapped to the colour camera.<br>
+                                        Good for overlaying on top of the colour camera.
+                                    </p>
+                                </a>
+                            </div>
+                        </td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    </div>
 </body>
 <footer id="footer"><?php echo execAndRead("{$SITE_ROOT}/assets/php/footer.php"); ?></footer>
 </html>
