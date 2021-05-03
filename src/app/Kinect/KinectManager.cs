052a723a065f04ab070dc55eaf9d12f24eb5fbaf
kOF.Readie <kof.readie@outlook.com>
2021-05-03 23:02:29 +0100

    WIP facial recognition

--- src/app/Kinect/KinectManager.cs
+++ src/app/Kinect/KinectManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
 
 namespace KinectOverWeb.Kinect
 {
@@ -15,6 +16,9 @@ namespace KinectOverWeb.Kinect
         private MultiSourceFrameReader reader;
         private IList<Body> bodies;
 
+        private FaceFrameSource faceSource;
+        private FaceFrameReader faceReader;
+
         public KinectManager()
         {
             frameSources = new FrameSources();
@@ -24,12 +28,51 @@ namespace KinectOverWeb.Kinect
             reader = kinect.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Body);
             reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
 
+            faceSource = new FaceFrameSource(kinect, 0,
+                FaceFrameFeatures.BoundingBoxInColorSpace |
+                FaceFrameFeatures.FaceEngagement |
+                FaceFrameFeatures.LookingAway |
+                FaceFrameFeatures.Glasses |
+                FaceFrameFeatures.Happy |
+                FaceFrameFeatures.LeftEyeClosed |
+                FaceFrameFeatures.RightEyeClosed |
+                FaceFrameFeatures.MouthOpen |
+                FaceFrameFeatures.MouthMoved |
+                FaceFrameFeatures.PointsInColorSpace |
+                FaceFrameFeatures.RotationOrientation
+            );
+            faceReader = faceSource.OpenReader();
+            faceReader.FrameArrived += FaceReader_FrameArrived;
+
             kinect.Open();
+        }
+
+        //https://pterneas.com/2014/12/21/kinect-2-face-basics/
+        private void FaceReader_FrameArrived(object sender, FaceFrameArrivedEventArgs _faceFrame)
+        {
+            using (FaceFrame frame = _faceFrame.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    Logger.Info(frame.IsTrackingIdValid);
+
+                    if (frame.FaceFrameResult != null)
+                    {
+                        string message = $"nose:{frame.FaceFrameResult.FacePointsInColorSpace[FacePointType.Nose]}" +
+                            $"happy:{frame.FaceFrameResult.FaceProperties[FaceProperty.Happy]}" +
+                            $"x:{frame.FaceFrameResult.FaceRotationQuaternion.X}" +
+                            $"y:{frame.FaceFrameResult.FaceRotationQuaternion.Y}" +
+                            $"z:{frame.FaceFrameResult.FaceRotationQuaternion.Z}" +
+                            $"w:{frame.FaceFrameResult.FaceRotationQuaternion.W}";
+                        Logger.Debug(message);
+                    }
+                }
+            }
         }
 
         private void Reader_MultiSourceFrameArrived(object _sender, MultiSourceFrameArrivedEventArgs _multiSourceFrame)
         {
-            var frame = _multiSourceFrame.FrameReference.AcquireFrame();
+            MultiSourceFrame frame = _multiSourceFrame.FrameReference.AcquireFrame();
 
             //I need to potomise this more before I add it into the program.
             /*if (frameSources.IsSourceEnabled(FrameSources.SourceTypes.Color))
